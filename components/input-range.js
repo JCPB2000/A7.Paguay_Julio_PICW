0b37de32c43b5f7aebb3090933d1af76f2b4bafe
JCPB2000 <jcpaguay1@espe.edu.ec>
2025-07-15 18:49:20 -0500

    Creacion de input-range

---
+++ components/input-range.js
@@ -0,0 +1,30 @@
+class InputRange extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    this.shadowRoot.innerHTML = `
+      <style>
+        label, input, button {
+          margin: 5px;
+          font-size: 16px;
+        }
+      </style>
+      <label for="inicio">Inicio:</label>
+      <input type="number" id="inicio" min="0">
+      <label for="fin">Fin:</label>
+      <input type="number" id="fin" min="0">
+      <button id="enviar">Enviar</button>
+    `;
+
+    this.shadowRoot.querySelector('#enviar')
+      .addEventListener('click', () => this.enviarRango());
+  }
+
+  enviarRango() {
+    const inicio = parseInt(this.shadowRoot.querySelector('#inicio').value);
+    const fin = parseInt(this.shadowRoot.querySelector('#fin').value);
+
+    
